55eed6cc0af1ead3383af15210c06f4d64d3acbc
Rickard Bengtsson <ricbe833@student.liu.se>
2022-12-04 08:17:21 +0100

    day4

---
+++ day4/part2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    stringstream ss;
+    string line;
+    int result, a_low, a_high, b_low, b_high;
+    char c;
+
+    while (getline(cin, line)) {
+        ss.str(line);
+        ss >> a_low >> c >> a_high >> c >> b_low >> c >> b_high;
+        ss.clear();
+        if ((a_low <= b_low && a_high >= b_low) || (b_low <= a_low && b_high >= a_low)) {
+            result++;
+        }
+    }
+    cout << result;
+    return 0;
+}
